86306a641e8bb64a40f6414384018ce1d3aba83b
Vitaliy Boyanivskyy <vitaliy.boyanivskiy@israelit.pro>
2020-04-18 15:30:39 +0300

    Changed deploy again

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + "/angular-build"));
+
+// Serve static files
+app.use(express.static(__dirname + "/dist/fe"));
+
+// Send all requests to index.html
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "angular-build", "index.html"));
+  res.sendFile(path.join(__dirname + "/dist/fe/index.html"));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
